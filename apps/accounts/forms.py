ed16993b92aeda855807efdc0eb4995525764bd5
Adel Yasser <devadel@tutamail.com>
2025-12-29 22:24:20 +0200

    feature(accounts): update account forms

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,14 +1,27 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
+    """Form for creating new users"""
     voucher_code = forms.CharField(
         required=False, 
         label="Voucher Code",
-        widget=forms.TextInput(attrs={'placeholder': 'Enter WELCOME2K for bonus', 'class': 'form-control'})
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter WELCOME2K for bonus', 
+            'class': 'form-control'
+        })
     )
+    
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
 
+
+class CustomUserChangeForm(UserChangeForm):
+    """Form for updating users in admin panel"""
+    
     class Meta:
         model = CustomUser
         fields = ("email",)
