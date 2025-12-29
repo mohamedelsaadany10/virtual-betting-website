8c4d52702f1e7829544da546a671bc7a6e8fa4b9
Adel Yasser <devadel@tutamail.com>
2025-12-29 21:11:07 +0200

    chore(config): include bets app URLs in main router

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.accounts.urls')),
+    path('wallet/', include('apps.wallet.urls')),     
+    path('bets/', include('apps.bets.urls')),        
 ]
